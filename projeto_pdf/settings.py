cc2e85d8368872259bcc7b1d1b94cb9ce1d54e93
AXS05000 <axssxs00@gmail.com>
2023-05-15 21:37:04 -0300

    V - 15-05-2023 - 21:37

--- projeto_pdf/settings.py
+++ projeto_pdf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x1&-5lg9ljeg&o%quyooi4xlf7p5ro+d=fzv^q3-*q!tqo2i&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -148,3 +148,14 @@ LOGIN_URL = 'login'
 SESSION_COOKIE_AGE = 604800  # 1 semana
 # ESSION_COOKIE_HTTPONLY = True
 # SESSION_COOKIE_SECURE = True
+
+
+#SECURE_HSTS_SECONDS = True
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#SECURE_CONTENT_TYPE_NOSNIFF = True
+#SUCURE_BROWSER_XSS_FILTER = True
+#CSRF_COOKIE_SECURE = True
+#CSRF_COOKIE_HTTPNLY = True
+#X_FRAME_OPTIONS = 'DENY'
+
+SECURE_SSL_REDIRECT = True
